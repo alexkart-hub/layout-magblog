7890bef50b08f88ae45bdd90e5acfd8a1623e83d
alexkart-hub <57880131+alexkart-hub@users.noreply.github.com>
2020-01-03 23:14:10 +0300

    Верстка для сайта блога

---
+++ js/canvas.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#myCanvas').tagcanvas({
+        textColour: '#00aedb', // Цвет текста 
+        outlineThickness: 1, // Обводка у ссылок (Да, Нет) 
+        maxSpeed: 0.05, // Максимальная скорость 
+        depth: 0.9 // Глубина. От 0 до 1 
+    })
+});
